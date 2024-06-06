b0b883f05639e3a1c1de2cf769f11927118a07d4
aliiane <160242832+aliiane@users.noreply.github.com>
2024-06-06 22:11:27 +0300

    Delete js/text_manipulation.js

--- js/text_manipulation.js
+++
@@ -1,7 +0,0 @@
-function camelToSnake(camel) {
-    return camel.replace(/([A-Z])/g, '_$1').toLowerCase();
-}
-
-function snakeToCamel(snake) {
-    return snake.replace(/(_\w)/g, (matches) => matches[1].toUpperCase());
-}
